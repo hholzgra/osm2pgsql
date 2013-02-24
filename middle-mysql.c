aba8f33b0f51f052ffeab685171b97d5bec319a8
Hartmut Holzgraefe <hartmut@php.net>
2013-02-24 16:07:13 +0100

    slowly adding flesh to the mysql middle layer

--- middle-mysql.c
+++ middle-mysql.c
@@ -8,41 +8,219 @@
 
 #include "config.h"
 
-#include <mysql.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+
+#include <mysql/mysql.h>
+#include "mysql.h"
 
 #include "osmtypes.h"
+
 #include "middle.h"
 #include "middle-mysql.h"
+#include "output.h"
 
+#include "node-ram-cache.h"
 
+static MYSQL sql_conn;
+static const struct output_options *my_options;
+
+static my_drop_tables(void)
+{
+  mysql_vexec(&sql_conn, "DROP TABLE IF EXISTS %s_nodes", my_options->prefix);
+  mysql_vexec(&sql_conn, "DROP TABLE IF EXISTS %s_ways", my_options->prefix);
+  mysql_vexec(&sql_conn, "DROP TABLE IF EXISTS %s_way_nodes", my_options->prefix);
+  mysql_vexec(&sql_conn, "DROP TABLE IF EXISTS %s_rels", my_options->prefix);
+  mysql_vexec(&sql_conn, "DROP TABLE IF EXISTS %s_rel_parts", my_options->prefix);
+}
 
 static int my_start(const struct output_options *options)
 {
+  my_options = options;
+
+  fprintf(stderr, "foo %p %p\n", options, my_options);
+
+  /* connect to mysql or exit_nicely() */
+  if (NULL == mysql_real_connect(&sql_conn, options->conn.host, options->conn.username, options->conn.password, options->conn.db, 3306 /* FIXME */, NULL, 0)) {
+    fprintf(stderr, "mysql middle connect failed: host %s:, db %s, user: %s, pwd: %s\n", options->conn.host, options->conn.db, options->conn.username, options->conn.password);
+    exit_nicely();
+  } else {
+    fprintf(stderr, "mysql middle connect succeeded\n");
+  }
+  
+  /* set up local ram caches */
+  init_node_ram_cache( options->alloc_chunkwise | ALLOC_LOSSY, options->cache, options->scale);
+  if (options->flat_node_cache_enabled) init_node_persistent_cache(options, options->append);
+    
+  if (!options->append) { 
+    /* drop and (re-)create tables */
+    my_drop_tables();
+
+    mysql_vexec(&sql_conn, "CREATE TABLE %s_nodes(id bigint not null primary key, lat int not null, lon int not null, tags text) ENGINE=myisam", options->prefix);
+
+    mysql_vexec(&sql_conn, "CREATE TABLE %s_ways(id bigint not null primary key, tags text, pending bool) ENGINE=myisam", options->prefix);
+
+    mysql_vexec(&sql_conn, "CREATE TABLE %s_way_nodes(way_id bigint not null, node_id bigint not null, sequence int not null, primary key(way_id, node_id, sequence)) ENGINE=myisam", options->prefix);
+
+    mysql_vexec(&sql_conn, "CREATE TABLE %s_rels(id bigint not null primary key, tags text, pending bool) ENGINE=myisam", options->prefix);
+
+    mysql_vexec(&sql_conn, "CREATE TABLE %s_rel_parts(rel_id bigint not null, part_id bigint not null, part_type char not null, role varchar(100), sequence int not null, primary key(rel_id, part_id, part_type, sequence)) ENGINE=myisam", options->prefix);
+
+    /* FIXME: ALTER TABLE ... DISABLE KEYS here? */
+  }
+
+  /* diagnostics */
+  fprintf(stderr, "Mid: mysql, scale=%d cache=%d\n", options->scale, options->cache);
+
   return 0;
 }
 
 static void my_stop(void) 
 {
+  /* shut down local ram caches */
+  free_node_ram_cache();
+  if (my_options->flat_node_cache_enabled) shutdown_node_persistent_cache();
+
+  /* FIXME: either enable keys and maybe analyze/optimize table */  
+
+  /* or drop table */
+  my_drop_tables();
+
+  /* close mysql connection */
+  mysql_close(&sql_conn);
 }
 
 static void my_cleanup(void)
 {
+  /* unused */
 }
 
 static void my_analyze(void)
 {
+  /* unused */
 }
 
 static void my_end(void) 
 {
+  /* unused */
 }
 
 static void my_commit(void) 
 {
+  /* just commit, simple as that here */
+  mysql_exec(&sql_conn, "COMMIT");
+}
+
+/* Special escape routine for escaping strings in array constants: double quote, backslash,newline, tab*/
+static char *escape_tag( char *ptr, const char *in, int escape )
+{
+  while( *in )
+  {
+    switch(*in)
+    {
+      case '"':
+        if( escape ) *ptr++ = '\\';
+        *ptr++ = '\\';
+        *ptr++ = '"';
+        break;
+      case '\'':
+        if( escape ) *ptr++ = '\\';
+        *ptr++ = '\\';
+        *ptr++ = '\'';
+        break;
+      case '\\':
+        if( escape ) *ptr++ = '\\';
+        if( escape ) *ptr++ = '\\';
+        *ptr++ = '\\';
+        *ptr++ = '\\';
+        break;
+      case '\n':
+        if( escape ) *ptr++ = '\\';
+        *ptr++ = '\\';
+        *ptr++ = 'n';
+        break;
+      case '\r':
+        if( escape ) *ptr++ = '\\';
+        *ptr++ = '\\';
+        *ptr++ = 'r';
+        break;
+      case '\t':
+        if( escape ) *ptr++ = '\\';
+        *ptr++ = '\\';
+        *ptr++ = 't';
+        break;
+      default:
+        *ptr++ = *in;
+        break;
+    }
+    in++;
+  }
+  return ptr;
+}
+
+/* escape means we return '\N' for copy mode, otherwise we return just NULL */
+static char *mysql_store_tags(struct keyval *tags, int escape)
+{
+  static char *buffer;
+  static int buflen;
+
+  char *ptr;
+  struct keyval *i;
+  int first;
+    
+  int countlist = countList(tags);
+  if( countlist == 0 )
+  {
+    if( escape )
+      return "\\N";
+    else
+      return NULL;
+  }
+    
+  if( buflen <= countlist * 24 ) /* LE so 0 always matches */
+  {
+    buflen = ((countlist * 24) | 4095) + 1;  /* Round up to next page */
+    buffer = realloc( buffer, buflen );
+  }
+_restart:
+
+  ptr = buffer;
+  first = 1;
+  *ptr++ = '{';
+  /* The lists are circular, exit when we reach the head again */
+  for( i=tags->next; i->key; i = i->next )
+  {
+    int maxlen = (strlen(i->key) + strlen(i->value)) * 4;
+    if( (ptr+maxlen-buffer) > (buflen-20) ) /* Almost overflowed? */
+    {
+      buflen <<= 1;
+      buffer = realloc( buffer, buflen );
+      
+      goto _restart;
+    }
+    if( !first ) *ptr++ = ',';
+    *ptr++ = '"';
+    ptr = escape_tag( ptr, i->key, escape );
+    *ptr++ = '"';
+    *ptr++ = ',';
+    *ptr++ = '"';
+    ptr = escape_tag( ptr, i->value, escape );
+    *ptr++ = '"';
+    
+    first=0;
+  }
+  
+  *ptr++ = '}';
+  *ptr++ = 0;
+  
+  return buffer;
 }
 
 static int my_nodes_set(osmid_t id, double lat, double lon, struct keyval *tags) 
 {
+  mysql_vexec(&sql_conn, "INSERT INTO %s_nodes SET id = %" PRIdOSMID ", lat = %.0f, lon = %.0f, tags = '%s'", my_options->prefix, id, lat, lon, mysql_store_tags(tags, 0));
+
   return 0;
 }
 
