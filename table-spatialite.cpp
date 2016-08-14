f3a01ffc4db0a048ef5a3d044a39f70bc9c0a7ab
Hartmut Holzgraefe <hartmut@php.net>
2016-08-14 12:42:55 +0200

    whitespace

--- table-spatialite.cpp
+++ table-spatialite.cpp
@@ -19,11 +19,11 @@ using std::string;
 typedef boost::format fmt;
 
 table_spatialite_t::table_spatialite_t(const database_options_t &database_options, const string& name, const string& type, const columns_t& columns, const hstores_t& hstore_columns,
-    const int srid, const bool append, const bool slim, const bool drop_temp, const int hstore_mode,
-    const bool enable_hstore_index, const boost::optional<string>& table_space, const boost::optional<string>& table_space_index) :
-    table_t(database_options, name, type, columns, hstore_columns, srid, append,
-   	    slim, drop_temp, hstore_mode, enable_hstore_index, table_space, table_space_index),
-    column_names()
+				       const int srid, const bool append, const bool slim, const bool drop_temp, const int hstore_mode,
+				       const bool enable_hstore_index, const boost::optional<string>& table_space, const boost::optional<string>& table_space_index) :
+  table_t(database_options, name, type, columns, hstore_columns, srid, append,
+	  slim, drop_temp, hstore_mode, enable_hstore_index, table_space, table_space_index),
+  column_names()
 {
   point_fmt = fmt("POINT(%.15g %.15g)");
 }
@@ -31,17 +31,17 @@ table_spatialite_t::table_spatialite_t(const database_options_t &database_option
 void table_spatialite_t::init_prepared_statements(void)
 {
   std::string insert_sql_wkt = (fmt("INSERT INTO %1% (%2%) VALUES (%3% GeomFromText(?, %4%))") % name % column_names % bind_names % srid).str();
-  
+
   if (SQLITE_OK != sqlite3_prepare_v2(sql_conn, insert_sql_wkt.c_str(), -1, &insert_stmt_wkt, 0)) {
     throw std::runtime_error((fmt("preparing stament '%1%' failed: %2%\n") % insert_sql_wkt % sqlite3_errmsg(sql_conn)).str());
   }
-  
+
   std::string insert_sql_wkb = (fmt("INSERT INTO %1% (%2%) VALUES (%3% GeomFromWKB(?, %4%))") % name % column_names % bind_names % srid).str();
-  
+
   if (SQLITE_OK != sqlite3_prepare_v2(sql_conn, insert_sql_wkb.c_str(), -1, &insert_stmt_wkb, 0)) {
     throw std::runtime_error((fmt("preparing stament '%1' failed: %2%\n") % insert_sql_wkb % sqlite3_errmsg(sql_conn)).str());
   }
-  
+
 }
 
 table_spatialite_t::table_spatialite_t(const table_spatialite_t& other):
@@ -55,14 +55,14 @@ table_spatialite_t::table_spatialite_t(const table_spatialite_t& other):
 
 table_spatialite_t::~table_spatialite_t()
 {
-    teardown();
+  teardown();
 }
 
 void table_spatialite_t::teardown()
 {
   if (conn_count > 0) {
     commit();
-    
+
     if (--conn_count == 0) {
       if(sql_conn != nullptr) {
 	sqlite3_close(sql_conn);
@@ -74,8 +74,8 @@ void table_spatialite_t::teardown()
 }
 
 std::string const& table_spatialite_t::get_name() {
-//TODO base classs?
-    return name;
+  //TODO base classs?
+  return name;
 }
 
 void table_spatialite_t::begin()
@@ -95,71 +95,87 @@ void table_spatialite_t::commit()
 
 void table_spatialite_t::start()
 {
-    connect();
-    commit();
-    
-    fprintf(stderr, "Setting up table: %s\n", name.c_str());
-    
-    if (!append)
+  connect();
+  commit();
+
+  fprintf(stderr, "Setting up table: %s\n", name.c_str());
+
+  if (!append)
     {
       simple_query((fmt("DROP TABLE IF EXISTS %1%") % name).str());
     }
 
-    /* These _tmp tables can be left behind if we run out of disk space */
-    simple_query((fmt("DROP TABLE IF EXISTS %1%_tmp") % name).str());
+  /* These _tmp tables can be left behind if we run out of disk space */
+  simple_query((fmt("DROP TABLE IF EXISTS %1%_tmp") % name).str());
 
-    if (!append)
+  if (!append)
     {
-        //define the new table
-        string sql = (fmt("CREATE TABLE %1% (`osm_id` BIGINT") % name).str();
+      //define the new table
+      string sql = (fmt("CREATE TABLE %1% (`osm_id` BIGINT") % name).str();
 
-	//first with the regular columns
-        for(columns_t::const_iterator column = columns.begin(); column != columns.end(); ++column) {
-	  sql += (fmt(",`%1%` %2%") % column->first % column->second).str();
-	}
+      //first with the regular columns
+      for(columns_t::const_iterator column = columns.begin(); column != columns.end(); ++column) {
+	sql += (fmt(",`%1%` %2%") % column->first % column->second).str();
+      }
 
-	// TODO: we don't support hstore columns yet
-
-	sql += ")";
-
-	simple_query(sql);
-
-	sql = (fmt("SELECT AddGeometryColumn('%1%', 'way', %2%, 'GEOMETRY', 'XY');") % name % srid).str();
+      //then with the hstore columns
+      for(hstores_t::const_iterator hcolumn = hstore_columns.begin(); hcolumn != hstore_columns.end(); ++hcolumn) {
+	sql += (fmt(", `%1%` TEXT") % (*hcolumn)).str();
+      }
 	
-	simple_query(sql);
+      //add tags column
+      if (hstore_mode != HSTORE_NONE) {
+	sql += ", `tags` TEXT";
+      }
 
-	//slim mode needs this to be able to delete from tables in pending
-	if (slim && !drop_temp) {
-	  std::string sql = (fmt("CREATE INDEX %1%_pkey ON %1% (osm_id)") % name).str();
-	  simple_query(sql);
-	}
+      sql += ")";
+
+      simple_query(sql);
+
+      sql = (fmt("SELECT AddGeometryColumn('%1%', 'way', %2%, 'GEOMETRY', 'XY');") % name % srid).str();
 	
-	simple_query((fmt("SELECT CreateSpatialIndex('%1%', 'way');") % name).str());
+      simple_query(sql);
+
+      //slim mode needs this to be able to delete from tables in pending
+      if (slim && !drop_temp) {
+	std::string sql = (fmt("CREATE INDEX %1%_pkey ON %1% (osm_id)") % name).str();
+	simple_query(sql);
+      }
+	
+      simple_query((fmt("SELECT CreateSpatialIndex('%1%', 'way');") % name).str());
     } //appending
-    else {
-      // TODO: check the columns against those in the existing table
-    }
+  else {
+    // TODO: check the columns against those in the existing table
+  }
 
-    //generate column list for INSERT
-    column_names = "`osm_id`,";
+  //generate column list for INSERT
+  column_names = "`osm_id`,";
 
-    //first with the regular columns
-    bind_names = "?,";
-    for(columns_t::const_iterator column = columns.begin(); column != columns.end(); ++column){
-      column_names += (fmt("`%1%`,") % column->first).str();
-      bind_names += "?,";
-    }
-    
-    // TODO: then with the hstore columns
-    // add tags column and geom column
-    // if (hstore_mode != HSTORE_NONE)
-    //    cols += "tags,";
+  //first with the regular columns
+  bind_names = "?,";
+  for(columns_t::const_iterator column = columns.begin(); column != columns.end(); ++column){
+    column_names += (fmt("`%1%`,") % column->first).str();
+    bind_names += "?,";
+  }
 
-    column_names += "`way`";
+  //then with the hstore columns
+  for(hstores_t::const_iterator hcolumn = hstore_columns.begin(); hcolumn != hstore_columns.end(); ++hcolumn) {
+    column_names += (fmt("`%1%`,") % (*hcolumn)).str();
+    bind_names += "?,";
+  }	
 
-    init_prepared_statements();
-    
-    begin();
+  // general --hstore column
+  if (hstore_mode != HSTORE_NONE) {
+    column_names += "`tags`,";
+    bind_names += "?,";
+  }
+
+  // geometry column
+  column_names += "`way`";
+
+  init_prepared_statements();
+
+  begin();
 }
 
 void table_spatialite_t::stop()
@@ -171,7 +187,7 @@ void table_spatialite_t::stop()
 
 void table_spatialite_t::write_node(const osmid_t id, const taglist_t &tags, double lat, double lon)
 {
-    write_row(id, tags, (point_fmt % lon % lat).str());
+  write_row(id, tags, (point_fmt % lon % lat).str());
 }
 
 void table_spatialite_t::delete_row(const osmid_t id)
@@ -181,57 +197,140 @@ void table_spatialite_t::delete_row(const osmid_t id)
 
 void table_spatialite_t::write_row(const osmid_t id, const taglist_t &tags, const std::string &geom)
 {
-    int n=1, stat;
+  int n=1, stat;
 
-    sqlite3_stmt *insert_stmt = (geom[0] > 'F') ? insert_stmt_wkt : insert_stmt_wkb;
+  sqlite3_stmt *insert_stmt = (geom[0] > 'F') ? insert_stmt_wkt : insert_stmt_wkb;
 
-    stat = sqlite3_bind_int(insert_stmt, n++, id);
-    if (SQLITE_OK != stat) {
-        throw std::runtime_error((fmt("bind_int failed: %1%\n") % stat).str());
+  // used to remember which columns have been written out already.
+  std::vector<bool> used;
+
+  if (hstore_mode != HSTORE_NONE) {
+    used.assign(tags.size(), false);
+  }
+
+  stat = sqlite3_bind_int(insert_stmt, n++, id);
+  if (SQLITE_OK != stat) {
+    throw std::runtime_error((fmt("bind_int failed: %1%\n") % stat).str());
+  }
+
+  // binding for regular columns
+  for(columns_t::const_iterator column = columns.begin(); column != columns.end(); ++column) {
+    int idx;
+
+    // bind tag value for column if present, otherwise bind NULL value
+    if ((idx = tags.indexof(column->first)) >= 0)
+      {
+	if (hstore_mode == HSTORE_NORM) {
+	  used[idx] = true;
+	}
+
+	if (SQLITE_OK != sqlite3_bind_text(insert_stmt, n++, tags[idx].value.c_str(), -1, SQLITE_STATIC)) {
+	  throw std::runtime_error((fmt("bind_text '%1%' failed for column %2%: %3%\n") % tags[idx].value % column->first % sqlite3_errmsg(sql_conn)).str());
+	}
+      } else {
+      if (SQLITE_OK != sqlite3_bind_null(insert_stmt, n++)) {
+	throw std::runtime_error((fmt("bind_null failed for column %1%: %2%\n") % column->first % sqlite3_errmsg(sql_conn)).str());
+      }
+    }
+  }
+
+  // binding for hstore columns
+  for(hstores_t::const_iterator hcolumn = hstore_columns.begin(); hcolumn != hstore_columns.end(); ++hcolumn) {
+    bool added = false;
+    std::string json = "{\"";
+
+    //iterate through the list of tags, first one is always null
+    for (taglist_t::const_iterator xtags = tags.begin(); xtags != tags.end(); ++xtags) {
+      //check if the tag's key starts with the name of the hstore column
+      if(xtags->key.compare(0, hcolumn->size(), *hcolumn) == 0)
+	{
+	  //generate the short key name, somehow pointer arithmetic works against the key string...
+	  const char* shortkey = xtags->key.c_str() + hcolumn->size();
+	
+	  //and pack the shortkey with its value into the json string
+	  if(added)
+	    json += "\",\"";
+	
+	  json += shortkey; // TODO escape?
+	  json += "\":\"";
+	
+	  escape4json(xtags->value, json);
+	
+	  //we did at least one so we need commas from here on out
+	  added = true;
+	}
     }
 
-    for(columns_t::const_iterator column = columns.begin(); column != columns.end(); ++column) {
-        int idx;
-
-        if ((idx = tags.indexof(column->first)) >= 0)
-        {
-            if (SQLITE_OK != sqlite3_bind_text(insert_stmt, n++, tags[idx].value.c_str(), -1, SQLITE_STATIC)) {
-                throw std::runtime_error((fmt("bind_text '%1%' failed: %2%\n") % tags[idx].value % sqlite3_errmsg(sql_conn)).str());
-            }
-        } else {
-            if (SQLITE_OK != sqlite3_bind_null(insert_stmt, n++)) {
-                throw std::runtime_error((fmt("bind_null failed '%1%' (%2%): %3%\n") % column->first % n % sqlite3_errmsg(sql_conn)).str());
-            }
-        }
-    }
-
-    // TODO hstore
-
-    if (insert_stmt == insert_stmt_wkt) {
-        sqlite3_bind_text(insert_stmt, n++, geom.c_str(), -1, SQLITE_STATIC); 
+    // bind tag value for column if present, otherwise bind NULL value
+    if (added) {
+      json.append("\"}");
+	
+      if (SQLITE_OK != sqlite3_bind_text(insert_stmt, n++, json.c_str(), -1, SQLITE_STATIC)) {
+	throw std::runtime_error((fmt("bind_text '%1%' failed for column %2%: %3%\n") % json % (*hcolumn) % sqlite3_errmsg(sql_conn)).str());
+      }
     } else {
-        char *bin;
-        int len = geom.length()/2;
+      if (SQLITE_OK != sqlite3_bind_null(insert_stmt, n++)) {
+	throw std::runtime_error((fmt("bind_null failed for %1% column: %2%\n") % (*hcolumn) % sqlite3_errmsg(sql_conn)).str());
+      }
+    }
+  }
 
-        bin = (char *)malloc(len);
-        boost::algorithm::unhex(geom, bin);
+  if (hstore_mode != HSTORE_NONE) {
+    std::string json = "{\"";
+    bool added = false;
 
-        sqlite3_bind_blob(insert_stmt, n++, bin, len, SQLITE_STATIC);
+    for (size_t i = 0; i < tags.size(); ++i) {
+      const tag_t& xtag = tags[i];
+      // skip z_order tag and keys which have their own column
+      if (used[i] || ("z_order" == xtag.key))
+	continue;
+	
+      if(added) {
+	json += "\",\"";
+      }
+	
+      json += xtag.key; // TODO: escape?
+      json += "\": \"";
+	
+      escape4json(xtag.value, json);
+	
+      //we did at least one so we need commas from here on out
+      added = true;
     }
 
-    if (SQLITE_DONE != sqlite3_step(insert_stmt)) {
-        throw std::runtime_error((fmt("step failed: %1%\n") % sqlite3_errmsg(sql_conn)).str());
+    json += "\"}";
+
+    if (SQLITE_OK != sqlite3_bind_text(insert_stmt, n++, json.c_str(), -1, SQLITE_TRANSIENT)) {
+      throw std::runtime_error((fmt("bind_text '%1%' failed for column tags: %2%\n") % json % sqlite3_errmsg(sql_conn)).str());
     }
-    if (SQLITE_OK != sqlite3_reset(insert_stmt)) {
-        throw std::runtime_error((fmt("reset failed: %1%\n") % sqlite3_errmsg(sql_conn)).str());
-    }
+  }
+
+  if (insert_stmt == insert_stmt_wkt) {
+    sqlite3_bind_text(insert_stmt, n++, geom.c_str(), -1, SQLITE_STATIC);
+  } else {
+    char *bin;
+    int len = geom.length()/2;
+
+    bin = (char *)malloc(len);
+    boost::algorithm::unhex(geom, bin);
+
+    sqlite3_bind_blob(insert_stmt, n++, bin, len, SQLITE_STATIC);
+  }
+
+  if (SQLITE_DONE != sqlite3_step(insert_stmt)) {
+    throw std::runtime_error((fmt("step failed: %1%\n") % sqlite3_errmsg(sql_conn)).str());
+  }
+  if (SQLITE_OK != sqlite3_reset(insert_stmt)) {
+    throw std::runtime_error((fmt("reset failed: %1%\n") % sqlite3_errmsg(sql_conn)).str());
+  }
+
 }
 
 
 table_t::wkb_reader table_spatialite_t::get_wkb_reader(const osmid_t id)
 {
   sqlite3_stmt *stmt;
-  
+
   std::string sql = (fmt("SELECT way FROM %1% WHERE osm_id = %2%") % name % id).str();
 
   sqlite3_prepare_v2(sql_conn, sql.c_str(), sql.length() + 1, &stmt, NULL);
@@ -246,13 +345,13 @@ void table_spatialite_t::connect()
 
     sqlite3_config(SQLITE_CONFIG_SERIALIZED);
     sqlite3_enable_shared_cache(1);
-    
+
     if ( SQLITE_OK  != sqlite3_open(database_options.db.c_str(), &_conn)) {
-        throw std::runtime_error((fmt("Connection to database failed: %1%\n") % sqlite3_errmsg(_conn)).str());
+      throw std::runtime_error((fmt("Connection to database failed: %1%\n") % sqlite3_errmsg(_conn)).str());
     }
-    
+
     sql_conn = _conn;
-    
+
     simple_query("SELECT load_extension('mod_spatialite.so');");
     simple_query("SELECT InitSpatialMetaData(1);");
     simple_query("PRAGMA read_uncommitted = True;");
@@ -265,66 +364,66 @@ void table_spatialite_t::connect()
 /* Escape data appropriate to the type */
 void table_spatialite_t::escape_type(const string &value, const string &type, string& dst) {
 
-    // For integers we take the first number, or the average if it's a-b
-    if (type == "int4") {
-        int from, to;
-        int items = sscanf(value.c_str(), "%d-%d", &from, &to);
-        if (items == 1)
-            dst.append((single_fmt % from).str());
-        else if (items == 2)
-            dst.append((single_fmt % ((from + to) / 2)).str());
-        else
-            dst.append("\\N");
-    }
-        /* try to "repair" real values as follows:
-         * assume "," to be a decimal mark which need to be replaced by "."
-         * like int4 take the first number, or the average if it's a-b
-         * assume SI unit (meters)
-         * convert feet to meters (1 foot = 0.3048 meters)
-         * reject anything else
-         */
-    else if (type == "real")
+  // For integers we take the first number, or the average if it's a-b
+  if (type == "int4") {
+    int from, to;
+    int items = sscanf(value.c_str(), "%d-%d", &from, &to);
+    if (items == 1)
+      dst.append((single_fmt % from).str());
+    else if (items == 2)
+      dst.append((single_fmt % ((from + to) / 2)).str());
+    else
+      dst.append("\\N");
+  }
+  /* try to "repair" real values as follows:
+   * assume "," to be a decimal mark which need to be replaced by "."
+   * like int4 take the first number, or the average if it's a-b
+   * assume SI unit (meters)
+   * convert feet to meters (1 foot = 0.3048 meters)
+   * reject anything else
+   */
+  else if (type == "real")
     {
-        string escaped(value);
-        std::replace(escaped.begin(), escaped.end(), ',', '.');
+      string escaped(value);
+      std::replace(escaped.begin(), escaped.end(), ',', '.');
 
-        float from, to;
-        int items = sscanf(escaped.c_str(), "%f-%f", &from, &to);
-        if (items == 1)
+      float from, to;
+      int items = sscanf(escaped.c_str(), "%f-%f", &from, &to);
+      if (items == 1)
         {
-            if (escaped.size() > 1 && escaped.substr(escaped.size() - 2).compare("ft") == 0)
-                from *= 0.3048;
-            dst.append((single_fmt % from).str());
+	  if (escaped.size() > 1 && escaped.substr(escaped.size() - 2).compare("ft") == 0)
+	    from *= 0.3048;
+	  dst.append((single_fmt % from).str());
         }
-        else if (items == 2)
+      else if (items == 2)
         {
-            if (escaped.size() > 1 && escaped.substr(escaped.size() - 2).compare("ft") == 0)
+	  if (escaped.size() > 1 && escaped.substr(escaped.size() - 2).compare("ft") == 0)
             {
-                from *= 0.3048;
-                to *= 0.3048;
+	      from *= 0.3048;
+	      to *= 0.3048;
             }
-            dst.append((single_fmt % ((from + to) / 2)).str());
+	  dst.append((single_fmt % ((from + to) / 2)).str());
         }
-        else
-            dst.append("\\N");
+      else
+	dst.append("\\N");
     }//just a string
-    else {
-      dst.append("'");
-      for (const char c: value) {
-	    switch(c) {
-	    case '\'':  dst.append("\'\'"); break;
-	    case '\\':  dst.append("\\\\"); break;
-	    //case 8:   dst.append("\\\b"); break;
-	    //case 12:  dst.append("\\\f"); break;
-	    case '\n':  dst.append("\\\n"); break;
-	    case '\r':  dst.append("\\\r"); break;
-	    case '\t':  dst.append("\\\t"); break;
-	    //case 11:  dst.append("\\\v"); break;
-	    default:    dst.push_back(c); break;
-            }
-	}
-      dst.append("'");
+  else {
+    dst.append("'");
+    for (const char c: value) {
+      switch(c) {
+      case '\'':  dst.append("\'\'"); break;
+      case '\\':  dst.append("\\\\"); break;
+	//case 8:   dst.append("\\\b"); break;
+	//case 12:  dst.append("\\\f"); break;
+      case '\n':  dst.append("\\\n"); break;
+      case '\r':  dst.append("\\\r"); break;
+      case '\t':  dst.append("\\\t"); break;
+	//case 11:  dst.append("\\\v"); break;
+      default:    dst.push_back(c); break;
+      }
     }
+    dst.append("'");
+  }
 }
 
 
@@ -332,10 +431,39 @@ void table_spatialite_t::escape_type(const string &value, const string &type, st
 void table_spatialite_t::simple_query(const std::string &sql)
 {
   //  fprintf(stderr, "SQL: %s\n", sql.c_str());
-  
+
   if (sqlite3_exec(sql_conn, sql.c_str(), 0, 0, 0)) {
     fprintf(stderr, "SQL Query: %s\n", sql.c_str());
     fprintf(stderr, "  Error %d: %s\n", sqlite3_errcode(sql_conn), sqlite3_errmsg(sql_conn));
     abort();
+  }
+}
+
+
+void table_spatialite_t::escape4json(const std::string& src_string, std:: string& dst)
+{
+  const char *src = src_string.c_str();
+
+  for (size_t i = 0; i < strlen(src); ++i) {
+    switch (src[i]) {
+    case '\\':
+      dst.append("\\\\x");
+      break;
+    case '"':
+      dst.append("\\\"");
+      break;
+    case '\t':
+      dst.append("\\\t");
+      break;
+    case '\r':
+      dst.append("\\\r");
+      break;
+    case '\n':
+      dst.append("\\\n");
+      break;
+    default:
+      dst.push_back(src[i]);
+      break;
+    }
   }
 }
--- table-spatialite.hpp
+++ table-spatialite.hpp
@@ -21,90 +21,92 @@ typedef std::vector<std::pair<std::string, std::string> > columns_t;
 
 class table_spatialite_t : public table_t
 {
-    public:
-        table_spatialite_t(const database_options_t &database_options, const std::string& name, const std::string& type, const columns_t& columns, const hstores_t& hstore_columns, const int srid,
-                const bool append, const bool slim, const bool droptemp, const int hstore_mode, const bool enable_hstore_index,
-                const boost::optional<std::string>& table_space, const boost::optional<std::string>& table_space_index);
-        table_spatialite_t(const table_spatialite_t& other);
-        ~table_spatialite_t();
+public:
+  table_spatialite_t(const database_options_t &database_options, const std::string& name, const std::string& type, const columns_t& columns, const hstores_t& hstore_columns, const int srid,
+		     const bool append, const bool slim, const bool droptemp, const int hstore_mode, const bool enable_hstore_index,
+		     const boost::optional<std::string>& table_space, const boost::optional<std::string>& table_space_index);
+  table_spatialite_t(const table_spatialite_t& other);
+  ~table_spatialite_t();
 
-        table_spatialite_t *clone() { return new table_spatialite_t(*this); }
-        void teardown();
-  
-        void start();
-        void stop();
+  table_spatialite_t *clone() { return new table_spatialite_t(*this); }
+  void teardown();
 
-        void begin();
-        void commit();
+  void start();
+  void stop();
 
-        void write_row(const osmid_t id, const taglist_t &tags, const std::string &geom);
-        void write_node(const osmid_t id, const taglist_t &tags, double lat, double lon);
-        void delete_row(const osmid_t id);
+  void begin();
+  void commit();
 
-        std::string const& get_name();
+  void write_row(const osmid_t id, const taglist_t &tags, const std::string &geom);
+  void write_node(const osmid_t id, const taglist_t &tags, double lat, double lon);
+  void delete_row(const osmid_t id);
 
-        static int conn_count;
-        static sqlite3 *sql_conn;
+  std::string const& get_name();
 
-    protected:
-  
-        struct sqlite_result_closer
-        {
-            void operator() (sqlite3_stmt *stmt)
-            {
-                sqlite3_finalize(stmt);
-            }
+  static int conn_count;
+  static sqlite3 *sql_conn;
 
-        };
+protected:
 
-    public:
-        //interface from retrieving well known binary geometry from the table
-        class wkb_reader_impl: public table_t::wkb_reader_impl
-        {
-            friend table_spatialite_t;
-            public:
-                const char* get_next()
-                {
-		     int stat;
-		       
-		     stat = sqlite3_step(m_stmt.get());
-		     if (stat == SQLITE_ROW) {
-		         return (const char *)sqlite3_column_text(m_stmt.get(), 0);
-		     } else {
-		         return nullptr;
-		     }
-		}
-                int get_count() const
-	        {
-	  	    // TODO remove
-		    return m_count;
-		}
-                void reset()
-                {
-		    // TODO implement ... but it is never used anyway?
-                }
-            private:
-                wkb_reader_impl(sqlite3_stmt *stmt)
-		  : m_stmt(stmt),
-		    m_count(0 /* TODO */),
-		    m_first(0 /* TODO */)
-                {}
+  struct sqlite_result_closer
+  {
+    void operator() (sqlite3_stmt *stmt)
+    {
+      sqlite3_finalize(stmt);
+    }
 
-                std::unique_ptr<sqlite3_stmt, sqlite_result_closer> m_stmt;
-                int m_count;
-                int m_first;
-        };
-  
-        table_t::wkb_reader get_wkb_reader(const osmid_t id);
-  
-    protected:
-        std::string column_names, bind_names;
-        sqlite3_stmt *insert_stmt_wkt;
-        sqlite3_stmt *insert_stmt_wkb;
-        void escape_type(const std::string &value, const std::string &type, std::string& dst);
-        void connect();
-        void simple_query(const std::string &sql);
-        void init_prepared_statements(void);
+  };
+
+public:
+  //interface from retrieving well known binary geometry from the table
+  class wkb_reader_impl: public table_t::wkb_reader_impl
+  {
+    friend table_spatialite_t;
+  public:
+    const char* get_next()
+    {
+      int stat;
+
+      stat = sqlite3_step(m_stmt.get());
+      if (stat == SQLITE_ROW) {
+	return (const char *)sqlite3_column_text(m_stmt.get(), 0);
+      } else {
+	return nullptr;
+      }
+    }
+    int get_count() const
+    {
+      // TODO remove
+      return m_count;
+    }
+    void reset()
+    {
+      // TODO implement ... but it is never used anyway?
+    }
+  private:
+    wkb_reader_impl(sqlite3_stmt *stmt)
+      : m_stmt(stmt),
+	m_count(0 /* TODO */),
+	m_first(0 /* TODO */)
+    {}
+
+    std::unique_ptr<sqlite3_stmt, sqlite_result_closer> m_stmt;
+    int m_count;
+    int m_first;
+  };
+
+  table_t::wkb_reader get_wkb_reader(const osmid_t id);
+
+protected:
+  std::string column_names, bind_names;
+  sqlite3_stmt *insert_stmt_wkt;
+  sqlite3_stmt *insert_stmt_wkb;
+  void escape_type(const std::string &value, const std::string &type, std::string& dst);
+  void connect();
+  void simple_query(const std::string &sql);
+  void init_prepared_statements(void);
+  void escape4json(const std::string&, std::string&);
+
 };
 
 #endif
